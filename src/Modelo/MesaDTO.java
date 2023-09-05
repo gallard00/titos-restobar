12f17dedef4a304c18c2e360128b4042ae4bdfbc
NahuDev <Nahuel.ng240@gmail.com>
2023-09-05 11:03:54 -0300

    TitosREstobar

---
+++ src/Modelo/MesaDTO.java
@@ -0,0 +1,23 @@
+package Modelo;
+
+public class MesaDTO {
+    private int id;
+    private String nombre;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
