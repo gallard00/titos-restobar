48af8381342494080c0f21cc868ca31037b57782
nicolassanchez311 <santeinformatica@gmail.com>
2023-10-03 05:31:04 -0600

    sanchez

--- src/View/FormIndex.java
+++ src/View/FormIndex.java
@@ -27,24 +27,24 @@ public class FormIndex extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jButtonMesas = new javax.swing.JButton();
+        jButtonProducto = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setText("TITO´S RESTOBAR");
 
-        jButton1.setText("MESAS");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jButtonMesas.setText("MESAS");
+        jButtonMesas.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jButtonMesasActionPerformed(evt);
             }
         });
 
-        jButton2.setText("PRODUCTOS");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        jButtonProducto.setText("PRODUCTOS");
+        jButtonProducto.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                jButtonProductoActionPerformed(evt);
             }
         });
 
@@ -54,37 +54,43 @@ public class FormIndex extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(91, 91, 91)
-                .addComponent(jButton1)
+                .addComponent(jButtonMesas)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 120, Short.MAX_VALUE)
-                .addComponent(jButton2)
+                .addComponent(jButtonProducto)
                 .addGap(56, 56, 56))
             .addGroup(layout.createSequentialGroup()
-                .addGap(148, 148, 148)
-                .addComponent(jLabel1)
+                .addGap(150, 150, 150)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(61, 61, 61)
+                .addGap(16, 16, 16)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(53, 53, 53)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addContainerGap(53, Short.MAX_VALUE))
+                    .addComponent(jButtonMesas)
+                    .addComponent(jButtonProducto))
+                .addContainerGap(42, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void jButtonMesasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonMesasActionPerformed
+       // Código que se ejecutará cuando se haga clic en el botón
+   FormMesa formMesa = new FormMesa ();
+    formMesa.setVisible(true);
+    this.dispose(); // Cierra el formulario actual si lo deseas
+    }//GEN-LAST:event_jButtonMesasActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void jButtonProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonProductoActionPerformed
+         // Código que se ejecutará cuando se haga clic en el botón
+    FormProducto formProducto = new FormProducto();
+    formProducto.setVisible(true);
+    this.dispose(); // Cierra el formulario actual si lo deseas
+    }//GEN-LAST:event_jButtonProductoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -122,8 +128,8 @@ public class FormIndex extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButtonMesas;
+    private javax.swing.JButton jButtonProducto;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
