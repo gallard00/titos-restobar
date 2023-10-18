a6a1032c136cd575deb14fff3ce6838b43930d88
nahuel1558 <nahuelzubiri@hotmail.es>
2023-10-18 20:53:25 -0300

    Nueva rama - Zubiri

--- src/View/FormIndex.java
+++ src/View/FormIndex.java
@@ -16,7 +16,7 @@ public class FormIndex extends javax.swing.JFrame {
     public FormIndex() {
         initComponents();
     }
-
+//hola
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
