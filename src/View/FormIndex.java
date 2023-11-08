94cbadc89ea4f5dfdc2a14e4d51ccde433993889
NahuDev <Nahuel.ng240@gmail.com>
2023-11-08 17:04:28 -0300

    Agregue comentarios al FormIndex

--- src/View/FormIndex.java
+++ src/View/FormIndex.java
@@ -74,42 +74,58 @@ public class FormIndex extends javax.swing.JFrame {
 
     private void btnMesasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMesasActionPerformed
         try {
+            // Comprueba si hay una ventana previa abierta y la cierra si es el caso
             if (ventanaActual != null) {
                 ventanaActual.dispose();// Cierra el formulario actual si existe uno abierto
             }
+            // Crea una nueva instancia de FormMesa
             FormMesa formMesa = new FormMesa();
+            // Añade un WindowListener para la ventana formMesa
             formMesa.addWindowListener(new WindowAdapter() {
                 @Override
+                // Define un método (cerrarVentana) que se espera que maneje el cierre de la ventana
                 public void windowClosed(WindowEvent e) {
                     // Acciones a realizar cuando se cierra FormMesa
                     setVisible(true); // Vuelve a mostrar el FormIndex al cerrar FormMesa
                 }
             });
+            // Muestra la ventana formMesa
             formMesa.setVisible(true);
-            ventanaActual = formMesa; // Establece el nuevo formulario como el actual
-            this.setVisible(false); // Oculta el FormIndex al abrir FormMesa
+            // Actualiza la referencia de ventana actual (ventanaActualProd) a formMesa
+            ventanaActual = formMesa;
+            // Oculta el FormIndex al abrir FormMesa
+            this.setVisible(false);
         } catch (SQLException ex) {
+            // Captura cualquier excepción SQLException que ocurra durante este proceso
             Logger.getLogger(FormIndex.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_btnMesasActionPerformed
 
     private void btnProductosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProductosActionPerformed
         try {
+            // Comprueba si hay una ventana previa abierta y la cierra si es el caso
             if (ventanaActual != null) {
                 ventanaActual.dispose();// Cierra el formulario actual si existe uno abierto
             }
+            // Crea una nueva instancia de FormProducto
             FormProducto formProducto = new FormProducto();
+            // Añade un WindowListener para la ventana formProducto
             formProducto.addWindowListener(new WindowAdapter(){
             @Override
+            // Define un método (cerrarVentana) que se espera que maneje el cierre de la ventana
                public void windowClosed(WindowEvent e) {
                    // Acciones a realizar cuando se cierra FormProducto
                    setVisible(true);// Vuelve a mostrar el FormIndex al cerrar FormProducto
                }
             });
+            // Muestra la ventana formProducto
             formProducto.setVisible(true);
-            ventanaActual = formProducto;// Establece el nuevo formulario como el actual
-            this.setVisible(false);// Oculta el FormIndex al abrir FormProducto
+            // Actualiza la referencia de ventana actual (ventanaActual) a formProducto
+            ventanaActual = formProducto;
+            // Oculta el FormIndex al abrir FormProducto
+            this.setVisible(false);
         } catch (SQLException ex) {
+            // Captura cualquier excepción SQLException que ocurra durante este proceso
             Logger.getLogger(FormIndex.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_btnProductosActionPerformed
